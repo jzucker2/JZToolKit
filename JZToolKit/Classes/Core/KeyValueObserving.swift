5de75ea5d746b90df712107f0dfee8d85cdf4e59
Jordan Zucker <jordan.zucker@gmail.com>
2017-02-17 16:39:57 -0800

    Updated to fix acl for new kvo feature

--- JZToolKit/Classes/Core/KeyValueObserving.swift
+++ JZToolKit/Classes/Core/KeyValueObserving.swift
@@ -14,10 +14,10 @@ public struct KVOActions: OptionSet {
     public init(rawValue: Int) {
         self.rawValue = rawValue
     }
-    static let add = KVOActions(rawValue: 1 << 0)
-    static let remove = KVOActions(rawValue: 1 << 1)
+    public static let add = KVOActions(rawValue: 1 << 0)
+    public static let remove = KVOActions(rawValue: 1 << 1)
     
-    static let all: KVOActions = [.add, .remove]
+    public static let all: KVOActions = [.add, .remove]
 }
 
 
