96fcdcdb160defb2e0e4be87f0a9de738ba3629d
Jordan Zucker <jordan.zucker@gmail.com>
2017-02-21 09:37:00 -0800

    Updated with beginnings of user data controller

---
+++ JZToolKit/Classes/CoreData/UserDataController.swift
@@ -0,0 +1,16 @@
+//
+//  UserDataController.swift
+//  Pods
+//
+//  Created by Jordan Zucker on 2/21/17.
+//
+//
+
+import UIKit
+import CoreData
+
+open class UserDataController: DataController {
+    
+    
+
+}
